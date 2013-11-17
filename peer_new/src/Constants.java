a4d4b78409a57c3093a4ff8d09e8d4dbb1154cd2
Rajath Prasad <rajathprasad@gmail.com>
2013-11-17 00:08:50 -0500

    2 nodes peering with central meta server

---
+++ peer_new/src/Constants.java
@@ -0,0 +1,9 @@
+import java.net.InetAddress;
+
+public class Constants {
+
+   public final static int SERVER_PORT = 3000;
+   public final static String SERVER_ADDRESS = "localhost";
+   public final static String CRLF = "\r\n";
+
+}
